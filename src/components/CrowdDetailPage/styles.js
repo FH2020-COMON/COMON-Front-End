99f20f2a93c12f416e0c075434fb28d5e5aeeef2
syi0808 <syi9397@naver.com>
2020-12-04 01:54:59 +0900

    [EDIT] CrowdDetailPage
    [CREATE] Bottom Scroll

--- src/components/CrowdDetailPage/styles.js
+++ src/components/CrowdDetailPage/styles.js
@@ -91,3 +91,59 @@ export const InputBox = styled.input`
     width: 100%;
     height: 380px;
 `
+
+export const BottomWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    background: #F4F4F4;
+    padding: 20px 0;
+    justify-content: center;
+`
+
+export const BottomHead = styled.div`   
+    width: 100%;
+    height: 35px;
+    border: 1px solid #707070;
+`
+
+export const ProjectWrapper = styled.div`
+    border-radius: 10px;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 1300px;
+`
+
+export const ProjectHeader = styled.p`
+    width: 200px;
+    padding: 8px 20px;
+    border-top-left-radius: 10px;
+    color: white;
+    font-size: 20px;
+    font-weight: 600;
+    text-align: center;
+    background: #7B00FF;
+    align-self: flex-start;
+    margin-bottom: 40px;
+`
+
+export const ProjectImg = styled.img`
+    width: calc(100% - 160px);
+    margin: 0 80px;
+    height: auto;
+`
+
+export const ProjectButton = styled.button`
+    border: 1px solid #7B00FF;
+    border-radius: 10px;
+    color: #7B00FF;
+    font-size: 24px;
+    font-weight: 600;
+    width: calc(100% - 100px);
+    margin: 50px 50px 20px;
+    background: white;
+    padding: 10px;
+    outline: none;
+    cursor: pointer;
+`
