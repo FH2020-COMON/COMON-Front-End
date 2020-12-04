5a34d5aad0d97ccad0b2be02c20c0bed19620299
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-12-04 19:53:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
       <BrowserRouter>
         <Route exact path="/" component={Main} />
         <Route exact path="/crowdfunding" component={CrowdFunding}></Route>
-
         <Route exact path="/asd" component={CrowdDetailPage} />
         <Route exact path="/qwer" component={CompanyApplyPage} />
         <Route exact path="/zxcv" component={CompanyNotice} />
