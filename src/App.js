b67eea718a8943826c4ad098437a918017d7867b
syi0808 <syi9397@naver.com>
2020-12-04 19:55:03 +0900

    [DELETE] App.js Route

--- src/App.js
+++ src/App.js
@@ -19,11 +19,6 @@ function App() {
       <BrowserRouter>
         <Route exact path="/" component={Main} />
         <Route exact path="/crowdfunding" component={CrowdFunding}></Route>
-
-        <Route exact path="/asd" component={CrowdDetailPage} />
-        <Route exact path="/qwer" component={CompanyApplyPage} />
-        <Route exact path="/zxcv" component={CompanyNotice} />
-        <Route exact path="/apply" component={CompanyApply} /> 
         <Route exact path="/mypage" component={Mypage} /> 
         <Route exact path="/crowddetail" component={CrowdDetailPage} />
         <Route exact path="/companyapply" component={CompanyApplyPage} />
