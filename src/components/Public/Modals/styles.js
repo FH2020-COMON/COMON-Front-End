da53032a20c40799d49d6cf56b4bd3892e544a36
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-12-04 10:11:42 +0900

    [ADD] Inveset Modal

--- src/components/Public/Modals/styles.js
+++ src/components/Public/Modals/styles.js
@@ -69,3 +69,76 @@ export const Login_Form=styled.form`
         margin-top: 10px;
     }
 `
+export const Invest_Form=styled.div`
+    display: flex;
+    flex-direction: column;
+    & input[type="number"]::-webkit-outer-spin-button,
+    & input[type="number"]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    & h4{
+        margin: 10px 0;
+        margin-top: 15px;
+        margin-left: 16px;
+        color: #333;
+    }
+`
+export const Invest_Submit=styled.div`
+    border :none;
+    background: #7B00FF;
+    color: white;
+    padding: 16px;
+    border-radius: 30px;
+    cursor: pointer;
+    font-size: 15px;
+    font-weight: bold;
+    margin-top: 30px;
+    text-align: center;
+`
+export const Money_State=styled.div`
+    display: flex;
+    align-items: center;
+    & span{
+        font-size: 30px;
+        padding: 16px;
+        color: #7B00FF;
+    }
+    & input{
+        border: none;
+        outline: none;
+        font-size: 20px;
+    }
+    & i {
+        margin-left: auto;
+        font-size: 18px;
+        color: #7B00FF;
+        cursor: pointer;
+        padding: 16px;
+    }
+`
+export const Money_box=styled.div`
+    display: flex;
+    justify-content: flex-end;
+    
+`
+export const Add_money=styled.button`
+    border: 1px solid #eee;
+    cursor: pointer;
+    border-radius: 5px;
+    margin: 0 10px;
+    padding: 5px 10px;
+    color: #333;
+    outline: none;
+    font-weight: bold;
+`
+export const Cheering_Message=styled.textarea`
+    outline: none;
+    border: none;
+    resize: none;
+    border-bottom: 2px solid #7B00FF;
+    border-radius: 5px;
+    height: 180px;
+    padding: 8px;
+    margin: 0 16px;
+`
